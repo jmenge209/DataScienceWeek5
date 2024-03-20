74a48a5bd7a38e0ae3989d084982686c724aed73
jmenge209 <164204226+jmenge209@users.noreply.github.com>
2024-03-20 08:10:16 -0700

    Create firstpython.py

---
+++ firstpython.py
@@ -0,0 +1,6 @@
+#My first python on Github
+#Ask user for current age and calculate/display their age next year
+age=int
+input("How old are you today?",age)
+print("You will be"&age&"next year)
+      
