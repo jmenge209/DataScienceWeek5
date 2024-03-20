fbf939fd94b2b78c5cb258cc4efe7c46f84bcda7
jmenge209 <164204226+jmenge209@users.noreply.github.com>
2024-03-20 08:55:10 -0700

    Create childbranch.py

---
+++ childbranch.py
@@ -0,0 +1,3 @@
+#test file to show how branches and pull requests function
+print("this is a test to show how pull requests function on child branches")
+print("this file should be approved and merged into parent branch")
